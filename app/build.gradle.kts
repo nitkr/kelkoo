6d4d47639d0bdb81c66c12b97caccfb914ca3647
nitkr <nithinkr09@gmail.com>
2026-09-06 16:11:43 +0000

    chore: bump version to 0.1.5 for NP Dash triad footer sideload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "app.kelkoo.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.4"
+        versionCode = 5
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
