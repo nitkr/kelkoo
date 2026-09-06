682def3d5907c888b46cd43c7cd570925ca6fe21
nitkr <nithinkr09@gmail.com>
2026-09-06 15:51:13 +0000

    chore: bump version to 0.1.4 for Explore header + Charts Echo scrub sideload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "app.kelkoo.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.3"
+        versionCode = 4
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
