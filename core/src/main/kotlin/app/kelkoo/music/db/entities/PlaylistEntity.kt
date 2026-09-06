5b0f0d598b567140f38f47418c163926d94f00ea
nitkr <nitkr@users.noreply.github.com>
2026-09-06 10:04:41 +0000

    brand: EQ-orbit launcher icon + YouTube share links
    Replace the basic K mark with the abstract equalizer/orbit icon.
    Share song/playlist/artist links use youtube.com instead of share.echomusic.fun.

--- core/src/main/kotlin/app/kelkoo/music/db/entities/PlaylistEntity.kt
+++ core/src/main/kotlin/app/kelkoo/music/db/entities/PlaylistEntity.kt
@@ -46,7 +46,7 @@ data class PlaylistEntity(
     val shareLink: String?
         get() {
             return if (browseId != null)
-                "https://share.echomusic.fun/playlist?list=$browseId"
+                "https://www.youtube.com/playlist?list=$browseId"
             else null
         }
 
