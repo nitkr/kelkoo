8898cf7a59d59d7a2c96464f6d7bfc662d423088
nitkr <nitkr@users.noreply.github.com>
2026-09-06 16:32:02 +0000

    chore: bump version to 0.1.6 for Settings Echo scrub sideload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "app.kelkoo.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.5"
+        versionCode = 6
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
